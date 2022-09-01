fd3f21e95ffb19a1ff228300149a87c925166fce
thays <thayscasado@hotmail.com>
2022-08-31 22:21:18 -0700

    adds route files using express router

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const connectDB = require('./config/db');
 // Connect Database
 connectDB();
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 // test endpoint
 app.get('/', (req, res) => res.send('API Running'));
 
