3a0cc51424b444029943bcf9389d332e4b9ab895
thays <thayscasado@hotmail.com>
2022-08-31 22:10:29 -0700

    adds mongodb configuration

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
 
 // test endpoint
 app.get('/', (req, res) => res.send('API Running'));
